d58b97b3d7e2297c5b57bfa71bbad962b7948226
iyangyuan <iyangyuan@outlook.com>
2014-04-27 19:12:43 +0800

    项目导入

--- README.md
+++ README.md
@@ -1,4 +1,141 @@
 ichat
 =====
-
-深度自由定制在线客服jQuery插件
+可能是历史上最灵活的jQuery在线客服插件
+=====
+  
+简介：
+-------------
+  
+`ichat`是一款开源免费在线客服`jQuery`插件，通过该插件，您可以自由的定制属于自己的在线客服代码。  
+`ichat`充分吸收传统在线客服插件的优点，并加上自身的独特设计，使得`ichat`可定制性异常强大。  
+`ichat`追求简单实用，走小清新路线，以便能够适应大多数网站风格。  
+`ichat`几乎全部由配置生成，使用简便，不需要写`html`，但需要写`js`，因为配置是`json`对象。  
+`ichat`体积小巧，代码高效，兼容性良好，采用`jQuery`插件封装，与原有系统零冲突。  
+  
+使用说明：
+-------------
+  
+引用插件的js、css文件：  
+  
+    <link rel="stylesheet" href="css/ichat-1.0.min.css">
+    <!--ichat是jQuery插件，必须先引入jQuery-->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/ichat-1.0.min.js"></script>
+  
+调用ichat插件，生成在线客服浮窗：  
+  
+    $("body").ichat({
+      //ichat配置
+    });
+  
+使用示例：
+-------------
+  
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>ichat使用示例</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/ichat-1.0.min.css">
+      </head>
+      <body>
+        
+        <script src="js/jquery-1.10.2.min.js"></script>
+        <script src="js/ichat-1.0.min.js"></script>
+        <script>
+          $(document).ready(function(){
+            $("body").ichat(
+              {
+                //浮窗位置，取值：left|right
+                placement: "right",
+                //浮窗到页面顶部的距离相对于浏览器高度的比例，取值：1~100
+                top: 20,
+                //浮窗显示的文本
+                title: "在线客服",
+                //浮窗配色，取值：black|blue|red|green
+                color: "blue",
+                //以何种事件展开浮窗，取值：click|hover
+                trigger: "click",
+                //浮窗展开后的可见内容列表
+                items:[
+                  {
+                    //内容类型，取值：qq|tel|text
+                    //qq用来显示客服QQ
+                    type: "qq",
+                    //内容标题
+                    title: "客服QQ",
+                    //内容以何种事件展开，取值：click|hover|none
+                    trigger: "click",
+                    //内容默认是否显示，取值：true|false
+                    open: true,
+                    //QQ列表
+                    items: [
+                      {
+                        //QQ名称
+                        title: "业务客服",
+                        //QQ号码
+                        qq: "1234567",
+                        //自定义QQ图标绝对地址，图标尺寸必须为16x16，如果不想使用自定义图标，请不要写此属性
+                        image: "http://www.kpdown.com/sexv/soft_.gif"
+                      },
+                      {
+                        title: "技术客服",
+                        qq: "1234568"
+                      },
+                      {
+                        title: "售后客服",
+                        qq: "1234569"
+                      },
+                    ]
+                  },{
+                    //tel用来显示联系电话
+                    type: "tel",
+                    title: "联系电话",
+                    trigger: "hover",
+                    open: false,
+                    //电话列表
+                    items: [
+                      {
+                        //作为超链接的title属性，鼠标移到电话上显示
+                        title: "总部号码",
+                        //电话号码，直接显示在列表中
+                        tel: "13200001111",
+                        //自定义电话图标绝对地址，图标尺寸必须为16x16，如果不想使用自定义图标，请不要写此属性
+                        image: "http://www.kpdown.com/sexv/soft_.gif"
+                      },{
+                        title: "朝阳区分机",
+                        tel: "13200001112"
+                      }
+                    ]
+                  },
+                  {
+                    //text用来显示文本内容
+                    //文本内容无图标，只有标题和内容
+                    type: "text",
+                    title: "其他信息",
+                    trigger: "none",
+                    open: true,
+                    items: [
+                      {
+                        //标题即文本的开头，加粗显示；如果不想指定标题，请不要写此属性
+                        title: "公告：",
+                        //文本内容，紧跟标题
+                        text: "关于服务器维护通知",
+                        //文本内容链接地址，如果不希望文本是超链接，请不要写此属性
+                        href: "http://www.baidu.com"
+                      },
+                      {
+                        title: "联系地址：",
+                        text: "北京市海淀区华强大厦Y座2-07B"
+                      }
+                    ]
+                  }
+                ]
+              }
+            );
+          });
+        </script>
+      </body>
+    </html>
+  
---
+++ js/ichat-1.0.js
@@ -0,0 +1,193 @@
+(function($){
+  $.fn.extend({
+    ichat: function(params){
+      //基础模版
+      var ichatBody = "<!-- ichat区域 -->"+
+      "<div id='ichatBody' class='ichat-body ichat-display' style=''>"+
+      "  <!-- ichat按钮 -->"+
+      "  <div id='ichatButton' class='ichat-button'>"+
+      "    <b id='ichatButtonName'></b>"+
+      "  </div>"+
+      "  <!-- ichat内容 -->"+
+      "  <div id='ichatContent' class='ichat-content'>"+
+      "  </div>"+
+      "</div>";
+      //列表基础模版
+      var itemBody = "<div>"+
+      "  <div data-ichat-toggle='@id' class='ichat-title'>@title</div>"+
+      "  <div id='@id' class='ichat-list'>"+
+      "  </div>"+
+      "</div>";
+      //分支业务逻辑
+      var logic = {
+        //浮窗逻辑
+        body: {
+          //浮窗位置
+          placement: {
+            left: function(){
+              $("#ichatBody").addClass("ichat-pull-left");
+              $("#ichatButton").addClass("ichat-float-right");
+              $("#ichatContent").addClass("ichat-float-left");
+            },
+            right: function(){
+              $("#ichatBody").addClass("ichat-pull-right");
+              $("#ichatButton").addClass("ichat-float-left");
+              $("#ichatContent").addClass("ichat-float-right");
+            }
+          },
+          //浮窗配色
+          color: {
+            black: function(){
+              $("#ichatButton").addClass("ichat-color-black");
+            },
+            blue: function(){
+              $("#ichatButton").addClass("ichat-color-blue");
+            },
+            red: function(){
+              $("#ichatButton").addClass("ichat-color-red");
+            },
+            green: function(){
+              $("#ichatButton").addClass("ichat-color-green");
+            }
+          },
+          //展开浮窗事件
+          trigger: {
+            click: function(){
+              $("#ichatButton").click(function(){
+                $("#ichatContent").toggle("slow");
+              });
+            },
+            hover: function(){
+              var _trigger;
+              $("#ichatBody").hover(function(){
+                //防止多次触发bug
+                _trigger = setTimeout(function(){
+                  $("#ichatContent").show("slow");
+                },200);
+              },function(){
+                clearTimeout(_trigger); 
+                $("#ichatContent").hide("slow");
+              });
+            }
+          }
+        },
+        //列表逻辑
+        items: {
+          qq: function(item){
+            //构造列表项
+            var qqOnline = "http://wpa.qq.com/msgrd?v=3&uin=@qq&site=qq&menu=yes";
+            var qqIco = "background-image: url('@url');";
+            var html = "<ul class='ichat-list-ul'>";
+            for(var i=0;i<item.items.length;i++){
+              var _s = item.items[i];
+              //是否自定义图标
+              if(_s.image){
+                html += "<li><a href='"+qqOnline.replace("@qq",_s.qq)+"' style=\""+qqIco.replace("@url",_s.image)+"\" target='_blank'>"+_s.title+"</a></li>";
+              }else{
+                html += "<li><a href='"+qqOnline.replace("@qq",_s.qq)+"' class='ichat-qq' target='_blank'>"+_s.title+"</a></li>";
+              }
+            }
+            html += "</ul>";
+            //填入页面
+            $("#"+item.id).html(html);
+          },
+          tel: function(item){
+            var telIco = "background-image: url('@url');";
+            var html = "<ul class='ichat-list-ul'>";
+            for(var i=0;i<item.items.length;i++){
+              var _s = item.items[i];
+              //是否自定义图标
+              if(_s.image){
+                html += "<li><a href='javascript:void(0);' title='"+_s.title+"' style=\""+telIco.replace("@url",_s.image)+"\">"+_s.tel+"</a></li>";
+              }else{
+                html += "<li><a href='javascript:void(0);' title='"+_s.title+"' class='ichat-tel'>"+_s.tel+"</a></li>";
+              }
+            }
+            html += "</ul>";
+            //填入页面
+            $("#"+item.id).html(html);
+          },
+          text: function(item){
+            var html = "";
+            for(var i=0;i<item.items.length;i++){
+              var _s = item.items[i];
+              //判断是否为超链接
+              if(_s.href){
+                html += "<p><b>"+_s.title+"</b><a href='"+_s.href+"' target='_blank'>"+_s.text+"</a></p>";
+              }else{
+                html += "<p><b>"+_s.title+"</b>"+_s.text+"</p>";
+              }
+            }
+            //填入页面
+            $("#"+item.id).html(html);
+          },
+          trigger: {
+            click: function(id){
+              $("div[data-ichat-toggle='"+id+"']").addClass("ichat-cursor");
+              $("div[data-ichat-toggle='"+id+"']").click(function(){
+                $("#"+id).slideToggle("slow");
+              });
+            },
+            hover: function(id){
+              var _trigger;
+              $("div[data-ichat-toggle='"+id+"']").addClass("ichat-cursor");
+              $("div[data-ichat-toggle='"+id+"']").parent().hover(function(){
+                //防止多次触发bug
+                _trigger = setTimeout(function(){
+                  $("#"+id).show("slow");
+                },200);
+              },function(){
+                clearTimeout(_trigger); 
+                $("#"+id).hide("slow");
+              });
+            },
+            none: function(id){
+              //什么也不做
+            }
+          }
+        }
+      };
+      
+      //主干业务逻辑
+      //1.构造基础框架
+      $(this).append(ichatBody);
+      $("#ichatBody").attr("style","top:"+params.top+"%;");
+      $("#ichatButtonName").text(params.title);
+      if(logic.body.placement[params.placement]){
+        logic.body.placement[params.placement]();
+      }
+      if(logic.body.color[params.color]){
+        logic.body.color[params.color]();
+      }
+      if(logic.body.trigger[params.trigger]){
+        logic.body.trigger[params.trigger]();
+      }
+      //2.构造列表
+      for(var i=0;i<params.items.length;i++){
+        var _s = params.items[i];
+        //1.构造基础结构
+        var _id = "ichatItem"+i;
+        var html = itemBody.replace(/@id/g,_id);
+        html = html.replace("@title",_s.title);
+        //填入页面
+        $("#ichatContent").append(html);
+        //2.判断默认显示状态
+        if(!_s.open){
+          $("#"+_id).addClass("ichat-display");
+        }
+        //3.生成列表
+        _s.id = _id;
+        //防止崩溃
+        if(logic.items[_s.type]){
+          logic.items[_s.type](_s);
+        }
+        //4.注册事件
+        if(logic.items.trigger[_s.trigger]){
+          logic.items.trigger[_s.trigger](_id);
+        }
+      }
+      //3.显示浮窗
+      $("#ichatBody").removeClass("ichat-display");
+    }
+  });
+})(jQuery);
